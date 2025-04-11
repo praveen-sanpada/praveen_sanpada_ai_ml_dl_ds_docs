5db3069c84b846aadc7d9b8fbdd63eec3bbb658a
Praveen Sanpada <praveen.sanpada@vinfotech.com>
2025-04-11 12:45:46 +0530

    code added

--- Step-by-Step Roadmap/18_Classification/10_AdaBoost_Classifier.py
+++
@@ -1,6 +0,0 @@
-# ✅ 9. AdaBoost Classifier
-# Boosts weak learners sequentially by focusing more on previous errors.
-from sklearn.ensemble import AdaBoostClassifier
-model = AdaBoostClassifier()
-model.fit(X_train, y_train)
-print("AdaBoost Accuracy:", model.score(X_test, y_test))
